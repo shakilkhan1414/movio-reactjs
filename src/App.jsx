208d71e86a31e2bc801aac5edf4c22e101dfec85
shakilkhan1414 <khan.shakil.1414@gmail.com>
2023-06-10 03:09:04 +0600

    add hero-slider and other sliders on home

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss'
 import Header from './components/header/Header'
 import { Footer } from './components/footer/Footer'
